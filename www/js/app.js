14311a6af8eae755853ee8cdf34b0b1694db93bf
Antonius Ostermann <ostermann.antonius@gmail.com>
2015-05-28 15:41:14 +0200

    adds cryptographic signatures to establish integrity and identity checks

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
 
 .run(function($ionicPlatform, pouchDB) {
+  console.log("asd");
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,160 +20,164 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+  });
 
-    /* for GUID generation */
-    guid = function() {
-      S4 = function()  {
-        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-      };
-      return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+  console.log("asd");
+
+  /* for GUID generation */
+  guid = function() {
+    S4 = function()  {
+      return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
     };
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+  };
 
-    /* Gets a sequence nr of a database name */
-    var getSeqNr = function (db, friend, callback) {
-      db = pouchDB(db);
-      db.info().then(function(r) {
-        callback(r.update_seq, friend);
+  /* Gets a sequence nr of a database name */
+  var getSeqNr = function (db, friend, callback) {
+    db = pouchDB(db);
+    db.info().then(function(r) {
+      callback(r.update_seq, friend);
+    });
+  }
+
+  /* Gets a friend by id with seq nr. if id == my id, returns my own info! */
+  var getFriend = function(id, basic, callback) {
+    // adds own data to friends array
+    new_basic = (JSON.parse(JSON.stringify(basic)));
+    new_basic.friends.push({id: basic.id, name: "me"});
+    for (friend in new_basic.friends) {
+      friend = new_basic.friends[friend];
+      if (friend.id == id) {
+        getSeqNr(id, friend, function(seq, friend){
+          friend.database = seq;
+          callback(friend);
+        });
+      }
+    }
+  };
+
+  /* Gets basic-data with seq data */
+  var basicWithSequence = function(basic, callback){
+    old_friends = basic.friends;
+    new_friends = [];
+    for (friend in old_friends) {
+      friend = old_friends[friend];
+      getFriend(friend.id, basic, function(f){
+        new_friends.push(f);
+        if (new_friends.length == old_friends.length){
+          new_basic = (JSON.parse(JSON.stringify(basic)));
+          new_basic.friends = new_friends;
+          callback(new_basic);
+        }
       });
     }
+  };
 
-    /* Gets a friend by id with seq nr. if id == my id, returns my own info! */
-    var getFriend = function(id, basic, callback) {
-      // adds own data to friends array
-      new_basic = (JSON.parse(JSON.stringify(basic)));
-      new_basic.friends.push({id: basic.id, name: "me"});
-      for (friend in new_basic.friends) {
-        friend = new_basic.friends[friend];
-        if (friend.id == id) {
-          getSeqNr(id, friend, function(seq, friend){
-            friend.database = seq;
-            callback(friend);
-          });
-        }
-      }
-    };
-
-    /* Gets basic-data with seq data */
-    var basicWithSequence = function(basic, callback){
-      old_friends = basic.friends;
-      new_friends = [];
-      for (friend in old_friends) {
-        friend = old_friends[friend];
-        getFriend(friend.id, basic, function(f){
-          new_friends.push(f);
-          if (new_friends.length == old_friends.length){
-            new_basic = (JSON.parse(JSON.stringify(basic)));
-            new_basic.friends = new_friends;
-            callback(new_basic);
-          }
-        });
-      }
-    };
-
-    /* Checks if a friend id is in friend list */
-    var inFriendList  = function(id, basic, callbackIfTrue) {
-      getFriend(id, basic, function(f) {
-        if (f != false) {callbackIfTrue(f);}
-      });
-    };
+  /* Checks if a friend id is in friend list */
+  var inFriendList  = function(id, basic, callbackIfTrue) {
+    getFriend(id, basic, function(f) {
+      if (f != false) {callbackIfTrue(f);}
+    });
+  };
 
 
-    /* Sends database changes to friend */
-    /* his_data and my_data needs to contain sequence data! */
-    var sendChanges = function(conn, his_data, my_data) {
-      /* Changes-Feed besorgen (auf Revision-Basis) und schicken */
-      db = pouchDB(his_data.id)
-      changes = db.changes({include_docs: true, since: his_data.database});
-      all_changes = [];
-      changes.on('change', function(c) {
-        all_changes.push(c);
-      }).on('complete', function() {
-        data = {changes: all_changes, id: my_data.id};
-        console.log("!!SENDING CHANGES:");
-        console.log(data);
-        conn.send(data); // Sends data to friend -> executes receiveChanges() on the other side!
-      });
-    };
-
-    /* Receives changes */
-    var receiveChanges = function(conn, data) {
-      console.log("!!RECEIVING CHANGES:");
+  /* Sends database changes to friend */
+  /* his_data and my_data needs to contain sequence data! */
+  var sendChanges = function(conn, his_data, my_data) {
+    /* Changes-Feed besorgen (auf Revision-Basis) und schicken */
+    db = pouchDB(his_data.id)
+    changes = db.changes({include_docs: true, since: his_data.database});
+    all_changes = [];
+    changes.on('change', function(c) {
+      all_changes.push(c);
+    }).on('complete', function() {
+      data = {changes: all_changes, id: my_data.id};
+      console.log("!!SENDING CHANGES:");
       console.log(data);
-      /* Feed-Daten empfangen: Was wurde geändert, aktualisiert, usw? */
-      db = pouchDB(data.id);
-      doc_changes = data.changes.map(function(v){return v.doc});
-      db.bulkDocs(doc_changes).then(function(r){
-        console.log("DB FOR ID = "+data.id+" IS NOW UP TO DATE!");
-        console.log("result = ");
-        console.log(r);
+      conn.send(data); // Sends data to friend -> executes receiveChanges() on the other side!
+    });
+  };
+
+  /* Receives changes */
+  var receiveChanges = function(conn, data) {
+    console.log("!!RECEIVING CHANGES:");
+    console.log(data);
+    /* Feed-Daten empfangen: Was wurde geändert, aktualisiert, usw? */
+    db = pouchDB(data.id);
+    doc_changes = data.changes.map(function(v){return v.doc});
+    db.bulkDocs(doc_changes).then(function(r){
+      console.log("DB FOR ID = "+data.id+" IS NOW UP TO DATE!");
+      console.log("result = ");
+      console.log(r);
+    });
+  };
+
+
+  /* Processes all sync requests */
+  var processSyncRequests = function(conn, request_data, basic) {
+    console.log(request_data);
+    cache = {};
+    for (friend in request_data.friends) {
+      friend = request_data.friends[friend];
+      cache[friend.id] = friend;
+      inFriendList(friend.id, basic, function(my_data){
+        his_data = cache[my_data.id]
+        if (his_data.database < my_data.database) {
+          sendChanges(conn, his_data, my_data);
+        }
       });
-    };
-
-
-    /* Processes all sync requests */
-    var processSyncRequests = function(conn, request_data, basic) {
-      console.log(request_data);
-      cache = {};
-      for (friend in request_data.friends) {
-        friend = request_data.friends[friend];
-        cache[friend.id] = friend;
-        inFriendList(friend.id, basic, function(my_data){
-          his_data = cache[my_data.id]
-          if (his_data.database < my_data.database) {
-            sendChanges(conn, his_data, my_data);
-          }
-        });
-      }
-    };
+    }
+  };
 
 
 
-    /* Function to call if database is ready => got my id and friends */
-    var databaseReady = function(basic) {
-      var peer = new Peer(basic.id, {key: 'awz3df7pmltedn29'});
+  /* Function to call if database is ready => got my id and friends */
+  var databaseReady = function(basic) {
+    var peer = new Peer(basic.id, {key: 'awz3df7pmltedn29'});
 
-      peer.on('open', function(id) {
-        console.log('My peer ID is: ' + id);
+    peer.on('open', function(id) {
+      console.log('My peer ID is: ' + id);
+      var connections = {};
 
-        /* Open connection for each friend */
-        var connections = [];
+      /* Open connection for each friend */
+      basicWithSequence(basic, function(basic_with_seq){
         for (friend in basic.friends) {
           friend = basic.friends[friend];
-          friend_connection = peer.connect(friend.id);
-          friend_connection.on('open', function(){
-
+          connections[friend.id] = peer.connect(friend.id);
+          console.log("ids should be equal: "+ friend.id);
+          connections[friend.id].on('open', function(){
+            current_connection = connections[this.peer];
             /* Send friend friends info (inc. seq nr) and my id */
-            basicWithSequence(basic, function(data) {
-              friend_connection.send(data);
-            });
+            current_connection.send(basic_with_seq);
 
             // Receive changes
-            friend_connection.on('data', function(data) {
-              receiveChanges(friend_connection, data);
+            current_connection.on('data', function(data) {
+              receiveChanges(connections[this.peer], data);
             });
           });
-          connections.push({
-            friend: friend,
-            connection: friend_connection
-          });
         }
+      });
 
-        /* Get new connection request (to send changes) */
-        peer.on('connection', function(conn) {
-          conn.on('data', function(request_data){
-            console.log(request_data);
-            processSyncRequests(conn, request_data, basic);
-          });
+      /* Get new connection request (to send changes) */
+      peer.on('connection', function(conn) {
+        connections[conn.peer] = conn;
+        conn.on('data', function(request_data){
+          processSyncRequests(connections[this.peer], request_data, basic);
         });
       });
-    };
+    });
+  };
 
 
-    /* Set initial values of database if unset */
-    var settings = pouchDB('settings');
-    var basic = {id: null, friends: null};
-    settings.get('my-own-id').then(function(doc){
-      basic.id = doc.value;
+  /* Set initial values of database if unset */
+  var settings = pouchDB('settings');
+  var basic = {id: null, friends: null, keyset: null};
+  console.log("ere");
+  settings.get('my-own-id').then(function(doc){
+    basic.id = doc.value;
+    console.log("s");
+    settings.get('keyset').then(function(doc) {
+      //basic.keyset = doc.value; dont save this for security reasons, just check it is there
       settings.get('friends').then(function(doc) {
         basic.friends = doc.value;
         databaseReady(basic);
@@ -182,16 +187,23 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
           databaseReady(basic);
         });
       });
-    }).catch(function(err){
-      if (err.status == 404) {
-        // -> There is no document with id in settings database, so generate one and put it in
-        settings.put({_id: 'my-own-id', value: guid()}).then(function(resp) {
-          window.location.reload();
-        });
-      }
+    }).catch(function(err) {
+      new_keyset = {public_key: null, private_key: null};
+      keypair = forge.pki.rsa.generateKeyPair({bits: 2048, e: 0x10001});
+      new_keyset.private_key = forge.pki.privateKeyToPem(keypair.privateKey);
+      new_keyset.public_key = forge.pki.publicKeyToPem(keypair.publicKey);
+      settings.put({_id: 'keyset', value: new_keyset}).then(function(resp){
+        basic.keyset = new_keyset;
+        window.location.reload();
+      });
     });
-
-
+  }).catch(function(err){
+    if (err.status == 404) {
+      // -> There is no document with id in settings database, so generate one and put it in
+      settings.put({_id: 'my-own-id', value: guid()}).then(function(resp) {
+        window.location.reload();
+      });
+    }
   });
 })
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,6 @@
+/* TODO: CHANGE TO https://github.com/digitalbazaar/forge#rsa */
+
+
 angular.module('starter.controllers', [])
 
 .controller('ChatsCtrl', function($scope, $ionicModal, pouchDB) {
@@ -5,17 +8,24 @@ angular.module('starter.controllers', [])
   settings.get('my-own-id').then(function(doc){
     $scope.own_id = doc.value;
     var my_database = pouchDB(doc.value);
-    my_database.allDocs({
-      include_docs: true
-    }).then(function (result) {
-      data = result.rows.map(function(val){return val.doc});
-      $scope.entries = data;
+    settings.get('keyset').then(function(doc){
+      $scope.keyset = doc.value;
+      my_database.allDocs({
+        include_docs: true
+      }).then(function (result) {
+        data = result.rows.map(function(val){return val.doc});
+        $scope.entries = data;
+      });
     });
 
 
     $scope.addEntry = function(){
       val = document.getElementById('new-entry').value;
-      my_database.put({_id: 'entry-'+Date.now(), content: val, date: Date.now()}).then(function(){
+      privateKey = forge.pki.privateKeyFromPem($scope.keyset.private_key);
+      hash = forge.md.sha1.create();
+      hash.update(val, "utf8");
+      signature = privateKey.sign(hash);
+      my_database.put({_id: 'entry-'+Date.now(), content: val, date: Date.now(), signature: signature}).then(function(){
         $scope.new_entry = "";
         window.location.reload();
       });
@@ -41,25 +51,54 @@ angular.module('starter.controllers', [])
 })
 
 .controller('FriendPinboardCtrl', function($scope, $stateParams, pouchDB) {
-  friendDB = pouchDB($stateParams.friendId);
-  friendDB.allDocs({
-    include_docs: true
-  }).then(function (result) {
-    data = result.rows.map(function(val){return val.doc});
-    $scope.entries = data;
+  settings = pouchDB("settings");
+  settings.get('friends').then(function(res){
+    friends = res.value;
+    public_key = null;
+    for(i=0; i<friends.length; i++){
+      if (friends[i].id == $stateParams.friendId){
+        public_key = friends[i].public_key;
+      }
+    }
+
+    public_key = forge.pki.publicKeyFromPem(public_key);
+    friendDB = pouchDB($stateParams.friendId);
+    friendDB.allDocs({
+      include_docs: true
+    }).then(function (result) {
+      data = result.rows.map(function(val){return val.doc});
+      entries = [];
+      for(i=0; i<data.length; i++){
+        entry = data[i];
+        hash = forge.md.sha1.create();
+        hash.update(entry.content, "utf8");
+        verified = public_key.verify(hash.digest().bytes(), entry.signature);
+        if (verified) {
+          entry.signature_okay = "JA";
+        }else{
+          entry.signature_okay = "NEIN";
+        }
+        entries.push(entry);
+      }
+      $scope.entries = entries;
+    });
+
   });
 })
 
 .controller('FriendCtrl', function($scope, $ionicModal, pouchDB) {
-  $scope.new_friend = {id: '', name: ''};
+  $scope.new_friend = {id: '', name: '', public_key: ''};
 
   var settings = pouchDB('settings');
   var friends_ref = "";
   settings.get('my-own-id').then(function(doc){
     $scope.own_id = doc.value;
-    settings.get('friends').then(function(doc) {
-      $scope.friends = doc.value;
-      friends_ref = doc._rev;
+    settings.get('keyset').then(function(doc){
+      $scope.public_key = doc.value.public_key;
+      settings.get('friends').then(function(doc) {
+        $scope.friends = doc.value;
+        friends_ref = doc._rev;
+      });
     });
   });
 
@@ -69,7 +108,7 @@ angular.module('starter.controllers', [])
     settings.put({_id: 'friends', value: $scope.friends, _rev: friends_ref}).then(function(resp){
       console.log(resp);
       friends_ref = resp._rev;
-      $scope.new_friend = {name: '', id: ''};
+      $scope.new_friend = {name: '', id: '', public_key: ''};
     }).catch(function(e){console.log(e)});
   };
 
